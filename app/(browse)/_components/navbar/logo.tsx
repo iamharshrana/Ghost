fab7e08c8475f838b1f1b3a9f34b48cceb133c72
harshfps <harshrana1552@gmail.com>
2024-01-18 23:09:35 +0530

    fixed typo

--- app/(browse)/_components/navbar/logo.tsx
+++ app/(browse)/_components/navbar/logo.tsx
@@ -13,7 +13,7 @@ export const Logo = () => {
     return (
         <Link href="/">
             <div className="flex lg:flex items-center gap-x-4 hover:opacity-75 transition">
-                <div className="bg-white rounded-full p-1 p-1 mr-12 shrink-0 lg:mr-0 lg:shrink-0">
+                <div className="bg-white rounded-full p-1  mr-12 shrink-0 lg:mr-0 lg:shrink-0">
                     <Image
                         src="/spooky.svg"
                         alt="ghost-theme"
